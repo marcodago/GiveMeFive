97185a818cc988b1d6e4771d94cb917ac6556870
Marco D'Agostino <marcodago@it.ibm.com>
2017-03-27 14:58:24 +0200

    1.3.5

--- GiveMeFive/RemoteNotificationService.swift
+++ GiveMeFive/RemoteNotificationService.swift
@@ -15,6 +15,9 @@ class RemoteNotificationService {
     
     static private let APPGUID = "50f67e2d-952b-41ea-83a6-ef0af87b2c3e"         // Internal CIO Bluemix instance
     static private let clientSecret = "fb50af74-50fd-4a50-9cab-f18072206644"    // Internal CIO Bluemix instance
+    
+//    static private let APPGUID = "236b4687-c1e6-4be7-97c4-6dc024d36d92"         // Internal Bluemix instance
+//    static private let clientSecret = "135f4436-e6e9-4a6c-8129-a8d3cebff702"    // Internal Bluemix instance
     static private let myBMSClient = BMSClient.sharedInstance
     static private let push = BMSPushClient.sharedInstance
     
@@ -26,8 +29,10 @@ class RemoteNotificationService {
     
     static func registerDeviceToken(_ deviceToken: Data) {
         push.initializeWithAppGUID(appGUID: APPGUID, clientSecret: clientSecret)
-        push.registerWithDeviceToken(deviceToken: deviceToken) { (response, statusCode, error) -> Void in
-            
+//        push.registerWithDeviceToken(deviceToken: deviceToken) { (response, statusCode, error) -> Void in
+      let userID = String( UserDefaults.standard.string(forKey: "googleuserid")!)
+      push.registerWithDeviceToken(deviceToken: deviceToken, WithUserId: userID) { response, statusCode, error in
+           
             if error.isEmpty {
                 print("Response during device registration : \(response)")
                 print("status code during device registration : \(statusCode)")
